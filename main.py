7bac129cbe112227935d6fee33ccc02ffa730a5a
sak3aq <125143118+sak3aq@users.noreply.github.com>
2025-05-04 00:05:36 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",   # ブラウザのタブに表示されるタイトル
+    page_icon=":material/home:",            # タブの左に表示されるアイコン（絵文字やURLでもOK）
+    layout="centered",       # レイアウト（"centered" または "wide"）
+    initial_sidebar_state="collapsed"  # サイドバーの初期状態（"auto", "expanded", "collapsed"）
+)
+
+if st.button("出演数集計アプリ"):
+    st.switch_page("pages/name_list_processor.py")
